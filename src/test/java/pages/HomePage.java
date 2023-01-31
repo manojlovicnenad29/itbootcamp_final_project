309ace2932b34b9fabe56a2499459aed7bed6bb2
Manojlovicnenad29 <manojlovicnenad29@gmail.com>
2023-01-31 20:22:18 +0100

    Created LogoutBotton in HomePage, and finished 4th test validLogin

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,19 @@
 package pages;
 
-public class HomePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class HomePage extends BasePage{
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]")
+    private WebElement logoutButton;
+
+
+    public HomePage(WebDriver driver, WebDriverWait driverWait) {
+        super(driver, driverWait);
+    }
+    public void logout(){
+        logoutButton.click();
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -9,19 +9,22 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.HomePage;
 import pages.LoginPage;
 
 public class LoginTests extends BaseTest {
 
     private LoginPage loginPage;
     private Faker faker;
+    private HomePage homePage;
 
     @Override
     @BeforeClass
     public void beforeClass() {
         super.beforeClass();
-        loginPage = new LoginPage(driver,driverWait);
+        loginPage = new LoginPage(driver, driverWait);
         faker = new Faker();
+        homePage = new HomePage(driver,driverWait);
     }
 
     @BeforeMethod
@@ -31,28 +34,51 @@ public class LoginTests extends BaseTest {
     }
 
     @Test
-    public void visitsTheLoginPage(){
+    public void visitsTheLoginPage() {
         String urlPage = "https://vue-demo.daniel-avellaneda.com/login";
-        Assert.assertEquals(driver.getCurrentUrl(),urlPage);
+        Assert.assertEquals(driver.getCurrentUrl(), urlPage);
     }
 
     @Test
-    public void checkInputTypes () {
+    public void checkInputTypes() {
         WebElement email = loginPage.getEmail();
         WebElement password = loginPage.getPassword();
         System.out.println(email);
-        Assert.assertEquals(email.getAttribute("type"),"email");
-        Assert.assertEquals(password.getAttribute("type"),"password");
+        Assert.assertEquals(email.getAttribute("type"), "email");
+        Assert.assertEquals(password.getAttribute("type"), "password");
     }
-@Test
-    public void DisplaysErrorsWhenUserDoesNotExist(){
-    String email = faker.internet().emailAddress();
-    String password = faker.internet().password();
-    loginPage.login(email,password);
-    String errorMsg = "User does not exists";
-    driverWait.until(ExpectedConditions.visibilityOf(loginPage.getErrorMsg()));
-    Assert.assertEquals(loginPage.getErrorMsg().getText(), errorMsg);
-}
+
+    @Test
+    public void DisplaysErrorsWhenUserDoesNotExist() {
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password();
+        loginPage.login(email, password);
+        String errorMsg = "User does not exists";
+        driverWait.until(ExpectedConditions.visibilityOf(loginPage.getErrorMsg()));
+        Assert.assertEquals(loginPage.getErrorMsg().getText(), errorMsg);
+        String urlPage = "https://vue-demo.daniel-avellaneda.com/login";
+        Assert.assertEquals(driver.getCurrentUrl(), urlPage);
+    }
+
+    @Test
+    public void displaysErrorsWhenPasswordIsWrong() {
+        String email = "admin@admin.com";
+        String password = faker.internet().password();
+        loginPage.login(email, password);
+        String errorMsg = "Wrong password";
+        Assert.assertEquals(loginPage.getErrorMsg().getText(), errorMsg);
+        String urlPage = "https://vue-demo.daniel-avellaneda.com/login";
+        Assert.assertEquals(driver.getCurrentUrl(), urlPage);
+    }
+
+    @Test
+    public void validLogin(){
+        loginPage.login("admin@admin.com","12345");
+        String expectedResult = "https://vue-demo.daniel-avellaneda.com/home";
+        WebElement logout = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]"));
+        driverWait.until(ExpectedConditions.elementToBeClickable(logout));
+        Assert.assertTrue(driver.getCurrentUrl().contains(expectedResult));
+    }
 
 
 }
