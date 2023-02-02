5e689b08ca25cdeb68fe9cff5c21e1b7342a9213
Manojlovicnenad29 <manojlovicnenad29@gmail.com>
2023-02-02 22:56:30 +0100

    Fisnihed 21th test (Locale test 2).

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,17 +5,50 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.swing.*;
+
 public class HomePage extends BasePage{
     @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]")
     private WebElement logoutButton;
 
+    @FindBy(className = "btnLocaleActivation")
+    private WebElement changeLanguageButton;
 
+    @FindBy(id = "list-item-75")
+    private WebElement esButton;
+
+    @FindBy(id = "list-item-73")
+    private WebElement enButton;
+
+    @FindBy(id = "list-item-77")
+    private WebElement frButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")
+    private WebElement header;
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
     public void logout(){
         // ovde treba proveriti.. tj ti si ulogovan, trebalo bi raditi, hmm
         logoutButton.click();
+    }
+    public void changeLanguage(){
+changeLanguageButton.click();
+    }
 
+    public WebElement getEsButton() {
+        return esButton;
+    }
+
+    public WebElement getEnButton() {
+        return enButton;
+    }
+
+    public WebElement getFrButton() {
+        return frButton;
+    }
+
+    public String getHeader() {
+        return header.getText();
     }
 }
--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -1,4 +1,34 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
 public class LocaleTests extends BaseTest{
+
+    private HomePage homePage;
+    @BeforeClass
+    public void beforeClass() {
+        super.beforeClass();
+        homePage = new HomePage(driver,driverWait);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        super.beforeMethod();
+    }
+@Test
+    public void setLocaleToES(){
+        homePage.changeLanguage();
+        homePage.getEsButton().click();
+    Assert.assertEquals(homePage.getHeader(),"Página de aterrizaje");
+}
+    @Test
+    public void setLocaleToEN(){
+        homePage.changeLanguage();
+        homePage.getEnButton().click();
+        Assert.assertEquals(homePage.getHeader(),"Landing");
+    }
 }
